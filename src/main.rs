b0c02e524edf4f34f5ffefd06b65da92ace20dc3
FatGuyy <omkarjadhav2103@gmail.com>
2023-11-18 08:12:17 +0530

    first test for bhejde commit comments

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ fn update_commit_push(comment: &str) {
 fn main() {
     let collect:Vec<String> = env::args().collect();
     let comment = &collect[1];
-    println!("comment is : {}", comment);
+    println!("{}", comment);
 
     update_commit_push(&comment);
 }
